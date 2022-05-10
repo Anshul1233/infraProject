245cb450861e20ad8d00f20055ad0688d0a161bf
Anshul <wakodikaranshul7@gmail.com>
2022-05-10 18:33:20 +0530

    add input and button tag

--- force-app/main/default/lwc/infraMain/infraMain.js
+++ force-app/main/default/lwc/infraMain/infraMain.js
@@ -1,3 +1,11 @@
 import { LightningElement } from 'lwc';
 
-export default class InfraMain extends LightningElement {}
+export default class InfraMain extends LightningElement {
+    name
+    flag = false
+
+    sendHandler(){
+        this.flag = true
+        this.name = this.template.querySelector('lightning-input').value;
+    }
+}
